e07159aa08df51eb1115b992d413bb993a42e674
MaxAngl <moutsilutz@gmail.com>
2025-11-30 22:18:07 +0100

    Implémentation du nouveau netket et log automatique

--- src/nqs_psc/trainfast.py
+++ src/nqs_psc/trainfast.py
@@ -1,5 +1,47 @@
 import netket as nk
+from nqs_psc.utils import save_run
+
+# Taille du système
+L = 3
+
+# Définition de l'hamiltonien
+
+g = nk.graph.Hypercube(length=L, n_dim=2, pbc=True)
+hi = nk.hilbert.Spin(s=1 / 2, N=g.n_nodes)
+ham = nk.operator.Ising(hi, g, h=3, J=1)
 
 
-def train():
-    print("Hello World netket")
+# Création de l'état variationnel
+
+model = nk.models.RBM()
+sampler = nk.sampler.MetropolisLocal(hi, n_chains=300)
+vstate = nk.vqs.MCState(sampler, model, n_samples=1000)
+
+# Optimisation
+
+optimizer = nk.optimizer.Sgd(learning_rate=0.01)
+gs = nk.driver.VMC(ham, optimizer, variational_state=vstate)
+
+# création du logger
+
+log = nk.logging.RuntimeLog()
+
+# One or more logger objects must be passed to the keyword argument `out`.
+gs.run(n_iter=300, out=log)
+
+
+meta = {
+    "L": L,
+    "graph": "Hypercube",
+    "n_dim": 2,
+    "pbc": True,
+    "hamiltonian": {"type": "Ising", "h": 3, "J": 1},
+    "model": "GCNN",
+    "sampler": {"type": "MetropolisLocal", "n_chains": 300, "n_samples": 1000},
+    "optimizer": {"type": "SGD", "lr": 0.01},
+    "n_iter": 300,
+}
+
+vstate_dict = vstate.to_dict()
+run_dir = save_run(log, meta, vstate_dict)
+print("test")
